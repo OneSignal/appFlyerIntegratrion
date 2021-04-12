ed424621c6898464df77c7ab30b291d788621b2b
emawby <emawby@gmail.com>
2021-04-12 10:32:19 -0700

    Fixing app to run and receive push on device
    Contains commented out code for UNUserNotificationCenterDelegate

--- AppFlyerOneSignal/AppDelegate.swift
+++ AppFlyerOneSignal/AppDelegate.swift
@@ -9,31 +9,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+        //UNUserNotificationCenter.current().delegate = self
+        
       
-      // Remove this method to stop OneSignal Debugging
-      OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+        // Remove this method to stop OneSignal Debugging
+        OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
 
-      // OneSignal initialization
-      OneSignal.initWithLaunchOptions(launchOptions)
-      OneSignal.setAppId("6d79d194-cd16-4a44-9735-c943e5ae81d4")
+        // OneSignal initialization
+        OneSignal.initWithLaunchOptions(launchOptions)
+        OneSignal.setAppId("6d79d194-cd16-4a44-9735-c943e5ae81d4")
+        
+        OneSignal.setLaunchURLsInApp(false)
 
-      // promptForPushNotifications will show the native iOS notification permission prompt.
-      // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
-      OneSignal.promptForPushNotifications(userResponse: { accepted in
+        // promptForPushNotifications will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
         print("User accepted notifications: \(accepted)")
-      })
-        
-//        if let deviceState = OneSignal.getDeviceState() {
-//            let customDataMap: [AnyHashable: Any] = [
-//              "custom_param_1" : "value_of_param_1"
-//            ]
-//            AppsFlyerLib.shared().customData = customDataMap
-//
-//            let deviceId = deviceState.userId
-//            customDataMap.put("onesignalCustomerId", deviceId);
-//            AppsFlyerLib.setAdditionalData(customData);
-//        }
-        
+        })
+
+        //        if let deviceState = OneSignal.getDeviceState() {
+        //            let customDataMap: [AnyHashable: Any] = [
+        //              "custom_param_1" : "value_of_param_1"
+        //            ]
+        //            AppsFlyerLib.shared().customData = customDataMap
+        //
+        //            let deviceId = deviceState.userId
+        //            customDataMap.put("onesignalCustomerId", deviceId);
+        //            AppsFlyerLib.setAdditionalData(customData);
+        //        }
+
         AppsFlyerLib.shared().appsFlyerDevKey = "pxcLfDgbcDHRsx6SJTcA7U"
         AppsFlyerLib.shared().appleAppID = "6d79d194-cd16-4a44-9735-c943e5ae81d4"
         AppsFlyerLib.shared().delegate = self
@@ -49,89 +54,97 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
             application.registerForRemoteNotifications()
         }
-        
+
         print("XXXXXXXXXXX AF 1 XXXXXXXXXX")
-        
+
         return true
     }
     
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-            // Start the SDK (start the IDFA timeout set above, for iOS 14 or later)
-            AppsFlyerLib.shared().start()
-            print("XXXXXXXXXXX AF 2 XXXXXXXXXX")
-        }
-        // Open Univerasal Links
-        // For Swift version < 4.2 replace function signature with the commented out code:
-        // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-        func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
-            print(" user info \(userInfo)")
-            print("XXXXXXXXXXX AF 3 XXXXXXXXXX")
-            AppsFlyerLib.shared().handlePushNotification(userInfo)
-        }
-        // Open Deeplinks
-        // Open URI-scheme for iOS 8 and below
-        private func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-            AppsFlyerLib.shared().continue(userActivity, restorationHandler: restorationHandler)
-            return true
-        }
-        // Open URI-scheme for iOS 9 and above
-        func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-            AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
-            return true
-        }
-        // Report Push Notification attribution data for re-engagements
-        func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-            AppsFlyerLib.shared().handleOpen(url, options: options)
-            return true
-        }
-        func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-            AppsFlyerLib.shared().handlePushNotification(userInfo)
-        }
-        // Reports app open from deep link for iOS 10 or later
-        func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-            AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
-            return true
-        }
+        // Start the SDK (start the IDFA timeout set above, for iOS 14 or later)
+        AppsFlyerLib.shared().start()
+        print("XXXXXXXXXXX AF 2 XXXXXXXXXX")
+    }
+    // Open Univerasal Links
+    // For Swift version < 4.2 replace function signature with the commented out code:
+    // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print(" user info \(userInfo)")
+        print("XXXXXXXXXXX AF 3 XXXXXXXXXX")
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
+    }
+//    // Open Deeplinks
+    // Open URI-scheme for iOS 8 and below
+    private func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: restorationHandler)
+        return true
+    }
+    // Open URI-scheme for iOS 9 and above
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
+        return true
+    }
+    // Report Push Notification attribution data for re-engagements
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, options: options)
+        return true
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
+    }
+    // Reports app open from deep link for iOS 10 or later
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+        return true
+    }
+//
+//    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+//        print("ECM: did receive response")
+//    }
+//
+//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//        print("ECM: will present notification")
+//    }
     
-    }
-    //MARK: AppsFlyerLibDelegate
-    extension AppDelegate: AppsFlyerLibDelegate{
-        // Handle Organic/Non-organic installation
-        func onConversionDataSuccess(_ installData: [AnyHashable: Any]) {
-            print("onConversionDataSuccess data:")
-            for (key, value) in installData {
-                print(key, ":", value)
-            }
-            if let status = installData["af_status"] as? String {
-                if (status == "Non-organic") {
-                    if let sourceID = installData["media_source"],
-                        let campaign = installData["campaign"] {
-                        print("This is a Non-Organic install. Media source: \(sourceID)  Campaign: \(campaign)")
-                    }
-                } else {
-                    print("This is an organic install.")
+}
+//MARK: AppsFlyerLibDelegate
+extension AppDelegate: AppsFlyerLibDelegate{
+    // Handle Organic/Non-organic installation
+    func onConversionDataSuccess(_ installData: [AnyHashable: Any]) {
+        print("onConversionDataSuccess data:")
+        for (key, value) in installData {
+            print(key, ":", value)
+        }
+        if let status = installData["af_status"] as? String {
+            if (status == "Non-organic") {
+                if let sourceID = installData["media_source"],
+                    let campaign = installData["campaign"] {
+                    print("This is a Non-Organic install. Media source: \(sourceID)  Campaign: \(campaign)")
                 }
-                if let is_first_launch = installData["is_first_launch"] as? Bool,
-                    is_first_launch {
-                    print("First Launch")
-                } else {
-                    print("Not First Launch")
-                }
+            } else {
+                print("This is an organic install.")
             }
-        }
-        func onConversionDataFail(_ error: Error) {
-            print(error)
-        }
-        //Handle Deep Link
-        func onAppOpenAttribution(_ attributionData: [AnyHashable : Any]) {
-            //Handle Deep Link Data
-            print("onAppOpenAttribution data:")
-            for (key, value) in attributionData {
-                print(key, ":",value)
+            if let is_first_launch = installData["is_first_launch"] as? Bool,
+                is_first_launch {
+                print("First Launch")
+            } else {
+                print("Not First Launch")
             }
-        }
-        func onAppOpenAttributionFailure(_ error: Error) {
-            print(error)
         }
     }
+    func onConversionDataFail(_ error: Error) {
+        print(error)
+    }
+    //Handle Deep Link
+    func onAppOpenAttribution(_ attributionData: [AnyHashable : Any]) {
+        //Handle Deep Link Data
+        print("onAppOpenAttribution data:")
+        for (key, value) in attributionData {
+            print(key, ":",value)
+        }
+    }
+    func onAppOpenAttributionFailure(_ error: Error) {
+        print(error)
+    }
+}
